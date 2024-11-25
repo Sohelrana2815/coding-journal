a2d368d85c070ebfdc86bdc59678428c143ecb9f
Sohelrana2815 <sohel152302@gmail.com>
2024-11-25 11:25:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h2 className="text-center font-bold text-4xl text-green-300">Hello World</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
